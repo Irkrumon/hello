a48e6e003a4ecd7a5f18c8e3c3da7b87ebf1aeaa
Muhammed-Ansil <ansil.p17@gmail.com>
2024-01-02 01:28:40 -0800

    finish

---
+++ src/component/Hero.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import './Hero.css';
+
+export default function Hero() {
+  return (
+    <div>
+      <div className='center'>
+     <p>
+      Creativity starts with viewing the 
+      </p>
+      <p> whole world differently</p>
+     </div>
+     <p className='para1'>A service agency with customer satisfaction</p>
+<div className='button'>   <button className='btn1'> Get Start</button> </div>
+
+<img src='https://i.pinimg.com/1200x/3f/fa/46/3ffa463173416a5eea503b949da5f56b.jpg' alt='' width="100%" />
+
+
+<p className='grow'>Grow your startup with our Service</p>
+
+<div className='cente'>
+<p className='build'>Build an incredible workplace and grow your business with Gusto’s </p>
+<p className='build'>all-in-one platform with amazing contents.</p>
+</div>
+
+
+<div className='three'>
+<div className='three1'>
+   <div> <img src='https://www.freepnglogos.com/uploads/advertising-png/digital-marketing-megaphone-advertising-5.png' alt='' height={90}/></div>
+   <div >
+     <p className='bottom head'>Marketing & advertising</p>
+     <p className='bottom'>Get your info tests delivered at </p>
+     <p className='bottom'>home collect a sample from the</p>
+     <p>your pogress tests.</p>
+   
+   </div>
+
+</div>
+<div className='three2'>
+<div> <img src='https://www.freepnglogos.com/uploads/advertising-png/digital-marketing-megaphone-advertising-5.png' alt='' height={90}/></div>
+   <div >
+     <p className='bottom head'>Ultimate development</p>
+     <p className='bottom'>Get your info tests delivered at </p>
+     <p className='bottom'>home collect a sample from the</p>
+     <p>your pogress tests.</p>
+   
+   </div>
+</div>
+<div className='three3'>
+<div> <img src='https://www.freepnglogos.com/uploads/advertising-png/digital-marketing-megaphone-advertising-5.png' alt='' height={90}/></div>
+   <div >
+     <p className='bottom head'>Business Enterprise</p>
+     <p className='bottom'>Get your info tests delivered at </p>,
+     <p className='bottom'>home collect a sample from the</p>
+     <p>your pogress tests.</p>
+   
+   </div>
+
+
+
+
+  
+</div>
+</div>
+<p> “Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a Qwilr Page we have made for them.”</p>
+
+    </div>
+
+  )
+}
---
+++ src/component/Nav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Nav.css';
+
+
+export default function Box1() {
+  return (
+    <div className='child'>
+      <div className='Nav'>
+<div className='space'>
+<div className='logo1'>
+    <img src='https://1000logos.net/wp-content/uploads/2022/07/Logo-ATT.png' alt='' width='px' height='27px'/>
+    <p>StartupLanding</p>
+</div>
+<div className='logo2'>
+    <p>Home</p>
+    <p>Adversite</p>
+    <p>Supports</p>
+    <p> About</p>
+</div>
+</div>
+<div className='logo3'>
+    <p className='p2'>login</p>
+    <p className='p1'>Get Start</p>
+</div>
+      </div>
+     
+
+      </div>
+  )
+}
